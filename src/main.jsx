6e40bf7dcfdd34619e82e6713a5ad9e6a942f0fb
ihaio <ihaio95@gmail.com>
2023-03-05 15:14:05 -0300

    bootstrap config

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./utils/error-page";
-import Clients from "./Pages/Clients/Clients";
-import Login from "./Pages/Login/Login";
-import Jobs from "./Pages/Jobs/Jobs";
-import Job from "./Pages/Jobs/Job";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Routes/Router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "react-bootstrap";
 
-import Home from "./Pages/Home/Home";
-import Client from "./Pages/Clients/Client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "clients",
-        element: <Clients />,
-      },
-      {
-        path: "clients/:id/edit",
-        element: <Client />,
-      },
-      {
-        path: "clients/tasks",
-        element: <Jobs />,
-      },
-      {
-        path: "clients/tasks/:id/edit",
-        element: <Job />,
-      },
-    ],
-  },
-  {
-    path: "login",
-    element: <Login />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider
+      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+      minBreakpoint="xxs"
+    >
+      <RouterProvider router={Router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
