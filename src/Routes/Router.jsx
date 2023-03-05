dab4f05986825c283fc9b5a62df344be007ccd85
ihaio <ihaio95@gmail.com>
2023-03-05 15:02:17 -0300

    create router file

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import AuthWrapper from "../utils/AuthWrapper";
+import ErrorPage from "../Pages/Error/Error";
+import Clients from "../Pages/Clients/Clients";
+import Login from "../Pages/Log/Login";
+import Jobs from "../Pages/Jobs/Jobs";
+import Job from "../Pages/Jobs/Job";
+import Home from "../Pages/Home/Home";
+import Client from "../Pages/Clients/Client";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AuthWrapper Component={Home} requiresAuth={true} />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "clients",
+        element: <AuthWrapper Component={Clients} requiresAuth={true} />,
+      },
+      {
+        path: "clients/:id/edit",
+        element: <AuthWrapper Component={Client} requiresAuth={true} />,
+      },
+      {
+        path: "clients/tasks",
+        element: <AuthWrapper Component={Jobs} requiresAuth={true} />,
+      },
+      {
+        path: "clients/tasks/:id/edit",
+        element: <AuthWrapper Component={Job} requiresAuth={true} />,
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <AuthWrapper Component={Login} requiresAuth={false} />,
+  },
+]);
+
+export default router;
