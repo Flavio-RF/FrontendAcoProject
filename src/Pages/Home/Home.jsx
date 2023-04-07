c6f8e62d9238f730883476a2259ccc18f1121d57
ihaio <ihaio95@gmail.com>
2023-04-07 10:06:13 -0300

    style: Add CSS styles to the Home component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,18 +2,19 @@ import { Outlet } from "react-router-dom";
 import { Col, Container, Row } from "react-bootstrap";
 import Nav from "../../components/Navbar";
 import Aside from "../../components/Aside";
+import "./home.css";
 
 function Home() {
   return (
     <Container fluid className="vh-100">
-      <Row>
+      <Row className="mb-5">
         <Nav />
       </Row>
-      <Row className="mt-5">
-        <Col sm={2} className="mt-5 d-flex justify-content-center">
+      <Row className="h-100">
+        <Col sm={1} className="position-sticky aside bg-dark">
           <Aside />
         </Col>
-        <Col sm={10} className="px-0 mx-0">
+        <Col sm={11} className="px-0 outlet">
           <Outlet />
         </Col>
       </Row>
